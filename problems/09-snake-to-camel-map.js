4e277efeab826fd7b56c7b9a101cb71927e01010
DoctorDoofen <and.bennett0930@gmail.com>
2024-03-30 15:57:35 -0500

    09 solved

--- problems/09-snake-to-camel-map.js
+++ problems/09-snake-to-camel-map.js
@@ -16,8 +16,17 @@ console.log(snakeToCamel('APp_ACADEMY_iS_cOol')); // 'AppAcademyIsCool'
 */
 
 function snakeToCamel(str) {
-    // Your code here 
+
+    let newWords = str.split('_');
+    let PascalCase = newWords.map((word) => {
+        return word[0].toUpperCase() + word.slice(1).toLowerCase();
+    })
+    return PascalCase.join('');
 }
+console.log(snakeToCamel('snakes_go_hiss')); // 'SnakesGoHiss'
+console.log(snakeToCamel('say_hello_world')); // 'SayHelloWorld'
+console.log(snakeToCamel('app_academy_is_cool')); // 'AppAcademyIsCool'
+console.log(snakeToCamel('APp_ACADEMY_iS_cOol')); // 'AppAcademyIsCool'
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
