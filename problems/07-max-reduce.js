31c1df63f89668420796b52fe0e2f405597f4086
DoctorDoofen <and.bennett0930@gmail.com>
2024-03-30 15:37:17 -0500

    07 solved

--- problems/07-max-reduce.js
+++ problems/07-max-reduce.js
@@ -14,9 +14,20 @@ console.log(maxWithReduce([-2, -3, -7, 3])); // 3
 
 */
 
-let maxWithReduce = function(nums) {
-    // Your code here 
+let maxWithReduce = function (nums) {
+
+  let number = nums.reduce((num, max) => {
+
+    let largestNum = num;
+    if (max > largestNum) {
+      largestNum = max;
+    }
+    return largestNum;
+  })
+  return number;
 };
+console.log(maxWithReduce([4, 6, 3, 5, 42, 4])); // 42
+console.log(maxWithReduce([-2, -3, -7, 3])); // 3
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
